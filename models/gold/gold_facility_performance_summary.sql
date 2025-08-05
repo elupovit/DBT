1900c37efda6cb8fe193cc62bf66ba3777c45095
Eitan Lupovitch <eitan.lupovitch@iqvia.com>
2025-08-05 22:46:51 +0000

    gold commit

---
+++ models/gold/gold_facility_performance_summary.sql
@@ -0,0 +1,17 @@
+SELECT 
+    se.facility_name,
+    se.state,
+    COUNT(*) as total_days,
+    ROUND(AVG(se.total_nursing_hours_per_patient), 2) as avg_hours_per_patient,
+    ROUND(AVG(ea.contract_percentage), 1) as avg_contract_percentage,
+    ROUND(AVG(wd.rn_percentage), 1) as avg_rn_percentage,
+    ROUND(AVG(sc.patient_census), 0) as avg_patient_census,
+    ROUND(AVG(wd.total_nursing_hours), 0) as avg_total_hours
+FROM {{ ref('silver_staffing_efficiency') }} se
+JOIN {{ ref('silver_employment_analysis') }} ea 
+    ON se.facility_id = ea.facility_id AND se.work_date = ea.work_date
+JOIN {{ ref('silver_workload_distribution') }} wd 
+    ON se.facility_id = wd.facility_id AND se.work_date = wd.work_date
+JOIN {{ ref('silver_nursing_staffing_cleaned') }} sc 
+    ON se.facility_id = sc.facility_id AND se.work_date = sc.work_date
+GROUP BY se.facility_name, se.state
---
+++ models/gold/gold_state_benchmarks.sql
@@ -0,0 +1,9 @@
+SELECT 
+    state,
+    COUNT(DISTINCT facility_id) as facility_count,
+    ROUND(AVG(total_nursing_hours_per_patient), 2) as state_avg_hours_per_patient,
+    ROUND(AVG(contract_percentage), 1) as state_avg_contract_pct
+FROM {{ ref('silver_staffing_efficiency') }} se
+JOIN {{ ref('silver_employment_analysis') }} ea USING (facility_id, work_date)
+GROUP BY state
+ORDER BY state_avg_hours_per_patient DESC
