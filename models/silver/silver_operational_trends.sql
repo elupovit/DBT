2cd059a269ca805535b20f0d9114cd30ce54f42a
Eitan Lupovitch <eitan.lupovitch@iqvia.com>
2025-08-05 22:33:22 +0000

    silver layer commit

---
+++ models/silver/silver_operational_trends.sql
@@ -0,0 +1,12 @@
+SELECT
+    facility_id,
+    facility_name,
+    state,
+    work_date,
+    EXTRACT(MONTH FROM work_date) as month,
+    EXTRACT(DOW FROM work_date) as day_of_week,
+    patient_census,
+    (rn_hours + lpn_hours + cna_hours) as total_nursing_hours,
+    ROUND((rn_hours + lpn_hours + cna_hours) / NULLIF(patient_census, 0), 2) as efficiency_ratio
+FROM {{ ref('silver_nursing_staffing_cleaned') }}
+WHERE patient_census > 0
---
+++ models/silver/silver_workload_distribution.sql
@@ -0,0 +1,14 @@
+SELECT 
+    facility_id,
+    facility_name,
+    state,
+    work_date,
+    rn_hours,
+    lpn_hours, 
+    cna_hours,
+    (rn_hours + lpn_hours + cna_hours) as total_nursing_hours,
+    ROUND(rn_hours / NULLIF((rn_hours + lpn_hours + cna_hours), 0) * 100, 1) as rn_percentage,
+    ROUND(lpn_hours / NULLIF((rn_hours + lpn_hours + cna_hours), 0) * 100, 1) as lpn_percentage,
+    ROUND(cna_hours / NULLIF((rn_hours + lpn_hours + cna_hours), 0) * 100, 1) as cna_percentage
+FROM {{ ref('silver_nursing_staffing_cleaned') }}
+WHERE (rn_hours + lpn_hours + cna_hours) > 0
